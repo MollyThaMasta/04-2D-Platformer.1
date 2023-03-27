5f6b0d88e4e1c63c9e0e5796845531200ae9b0b0
BL-MSCH-C220-F22 <121530054+BL-MSCH-C220@users.noreply.github.com>
2023-03-27 11:05:48 -0400

    Adds coin sound and fixes quit button

--- Coin.gd
+++ Coin.gd
@@ -8,6 +8,9 @@ func _ready():
 
 func _on_Coin_body_entered(body):
 	if body.name == "Player":
+		var sound = get_node_or_null("/root/Game/Coin_sound")
+		if sound != null:
+			sound.playing = true
 		Global.increase_score(score)
 		queue_free()
 	
